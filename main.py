406a0d131424be56971064694d0b64dd22e96897
nglabs42 <86898778+nglabs42@users.noreply.github.com>
2022-09-24 08:16:08 -0500

    updated expected white space

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def arg_collection():
     args = parser.parse_args()
     return args
 
+
 def setup_logging(loglevel):
     """Setup basic logging
 
@@ -32,11 +33,12 @@ def setup_logging(loglevel):
         filename="/var/log/namerdbin.log",
     )
 
+
 def main():
     """Main function"""
 
     our_arguments = arg_collection()
-    #collect filename and output file from argparse
+    # collect filename and output file from argparse
     filename = our_arguments.input
     outputfile = our_arguments.output
 
@@ -47,22 +49,23 @@ def main():
         LOGGER.error("Input File not found %s", error)
         sys.exit(1)
 
-    data=[x.strip() for x in data]
+    data = [x.strip() for x in data]
 
-    #if data is empty log error and exit
+    # if data is empty log error and exit
     if not data:
         LOGGER.error("Input File is empty")
         sys.exit(1)
 
     our_translated = dict()
-    done={}
-    templst=[]
+    done = {}
+    templst = []
     for x in data:
         try:
-            translate =idna.decode(x)
-            translate=translate.split(",")[0]
-            state=subprocess.getoutput(f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
-            reserved=subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
+            translate = idna.decode(x)
+            translate = translate.split(",")[0]
+            state = subprocess.getoutput(
+                f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
+            reserved = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
             hoursUntilBidding = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilBidding")
             hoursUntilReveal = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .stats.hoursUntilReveal")
             hoursUntilClose = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilClose")
@@ -75,8 +78,8 @@ def main():
                 templst.append(hoursUntilReveal)
                 templst.append(hoursUntilClose)
                 templst.append(daysUntilExpire)
-                our_translated[x]=templst
-                templst=[]
+                translated[x] = templst
+                templst = []
             else:
                 templst.append(translate)
                 templst.append(state)
@@ -85,14 +88,15 @@ def main():
                 templst.append(hoursUntilReveal)
                 templst.append(hoursUntilClose)
                 templst.append(daysUntilExpire)
-                translated[x]=templst
-                templst=[]
+                translated[x] = templst
+                templst = []
         except idna.InvalidCodepoint as e:
-            elements=e.args
-            translate=elements[0].split("\'")[1]
+            elements = e.args
+            translate = elements[0].split("\'")[1]
             templst.append(translate)
-            state=subprocess.getoutput(f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
-            reserved=subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
+            state = subprocess.getoutput(
+                f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
+            reserved = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
             hoursUntilBidding = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilBidding")
             hoursUntilReveal = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .stats.hoursUntilReveal")
             hoursUntilClose = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilClose")
@@ -105,8 +109,8 @@ def main():
                 templst.append(hoursUntilReveal)
                 templst.append(hoursUntilClose)
                 templst.append(daysUntilExpire)
-                translated[x]=templst
-                templst=[]
+                translated[x] = templst
+                templst = []
             else:
                 templst.append(translate)
                 templst.append(state)
@@ -115,14 +119,15 @@ def main():
                 templst.append(hoursUntilReveal)
                 templst.append(hoursUntilClose)
                 templst.append(daysUntilExpire)
-                translated[x]=templst
-                templst=[]
+                translated[x] = templst
+                templst = []
         except idna.InvalidCodepoint as e:
-            elements=e.args
-            translate=elements[0].split("\'")[1]
+            elements = e.args
+            translate = elements[0].split("\'")[1]
             templst.append(translate)
-            state=subprocess.getoutput(f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
-            reserved=subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
+            state = subprocess.getoutput(
+                f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
+            reserved = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
             hoursUntilBidding = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilBidding")
             hoursUntilReveal = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .stats.hoursUntilReveal")
             hoursUntilClose = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilClose")
@@ -133,11 +138,12 @@ def main():
             templst.append(hoursUntilReveal)
             templst.append(hoursUntilClose)
             templst.append(daysUntilExpire)
-            translated[x]=templst
-            templst=[]
+            translated[x] = templst
+            templst = []
         except Exception as e:
-            state=subprocess.getoutput(f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
-            reserved=subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
+            state = subprocess.getoutput(
+                f"hsd-cli --api-key=81cd9577-0415-4219-9726-cdff1ebb2039 rpc getnameinfo {x}|jq .info.state")
+            reserved = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .start.reserved")
             hoursUntilBidding = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilBidding")
             hoursUntilReveal = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .stats.hoursUntilReveal")
             hoursUntilClose = subprocess.getoutput(f"hsd-cli rpc getnameinfo {x}|jq .info.stats.hoursUntilClose")
@@ -149,28 +155,29 @@ def main():
             templst.append(hoursUntilReveal)
             templst.append(hoursUntilClose)
             templst.append(daysUntilExpire)
-            translated[x]=templst
-            templst=[]
+            translated[x] = templst
+            templst = []
 
-    translated={"name":translated.keys()
-                ,"decoded_punycode":[a[0] for a in translated.values()]
-                ,"status":[a[1] for a in translated.values()]
-                ,"reserved":[a[2] for a in translated.values()]
-                ,"hoursUntilBidding":[a[3] for a in translated.values()]
-                ,"hoursUntilReveal":[a[4] for a in translated.values()]
-                ,"hoursUntilClose":[a[5] for a in translated.values()]
-                ,"daysUntilExpire":[a[6] for a in translated.values()]}
-    nlst=[]
+    translated = {"name": translated.keys()
+        , "decoded_punycode": [a[0] for a in translated.values()]
+        , "status": [a[1] for a in translated.values()]
+        , "reserved": [a[2] for a in translated.values()]
+        , "hoursUntilBidding": [a[3] for a in translated.values()]
+        , "hoursUntilReveal": [a[4] for a in translated.values()]
+        , "hoursUntilClose": [a[5] for a in translated.values()]
+        , "daysUntilExpire": [a[6] for a in translated.values()]}
+    nlst = []
 
     for key, value in translated.items():
         for i in value:
             nlst.append(i)
-        done[key]=nlst
-        nlst=[]
+        done[key] = nlst
+        nlst = []
 
-    df =pd.DataFrame(done)
-    df.to_csv(outputfile,index=None,encoding ="utf-16",sep=",")
+    df = pd.DataFrame(done)
+    df.to_csv(outputfile, index=None, encoding="utf-16", sep=",")
     LOGGER.info("Done!")
+
 
 if __name__ == "__main__":
     """This is executed when run from the command line"""
